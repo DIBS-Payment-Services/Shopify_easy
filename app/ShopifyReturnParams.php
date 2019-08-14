bdff0364676c9cc54319b8e78d7249059f9ea1f9
maxwhite <maxwhite1983@gmail.com>
2019-08-14 19:03:53 +0300

    setting flow not finished 2

--- app/ShopifyReturnParams.php
+++ app/ShopifyReturnParams.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App;
 
 /**
@@ -14,55 +8,63 @@ namespace App;
  * @author mabe
  */
 class ShopifyReturnParams {
-    
+
     private $params = [];
-    
+
     public function setX_Amount($amount) {
         $this->params['x_amount'] = $amount;
     }
-    
+
     public function setX_Currency($currency) {
         $this->params['x_currency'] = $currency;
     }
-    
+
     public function setX_GatewayReference($gatewayReference) {
         $this->params['x_gateway_reference'] = $gatewayReference;
     }
-    
+
     public function setX_Reference($reference) {
         $this->params['x_reference'] = $reference;
     }
-    
+
     public function setX_Result($result) {
         $this->params['x_result'] = $result;
     }
-    
+
     public function setX_Timestamp($timestamp) {
         $this->params['x_timestamp'] = $timestamp;
     }
-    
+
     public function setX_TransactionType($transactionType) {
         $this->params['x_transaction_type'] = $transactionType;
     }
-    
+
     public function setX_AccountId($accountId) {
         $this->params['x_account_id'] = $accountId;
     }
-    
+
     public function setX_Test() {
         $this->params['x_test'] = 'true';
     }
-    
+
     public function setX_Signature($signature) {
         $this->params['x_signature'] = $signature;
     }
-    
-    public function setX_Message() {
-        $this->params['x_message'] = '';
+
+    public function setX_Message($message) {
+        $this->params['x_message'] = $message;
     }
-    
+
+    public function setX_CardType($field) {
+        $this->params['x_card_type'] = $field;
+    }
+
+    public function setX_CardMaskedPan($field) {
+        $this->params['x_card_masked_pan'] = $field;
+    }
+
     public function getParams() {
         return $this->params;
     }
-    
+
 }
--- resources/views/easy-settings-form.blade.php
+++ resources/views/easy-settings-form.blade.php
@@ -8,9 +8,12 @@
   </head>
   <body>
     <div id="root"></div>
+  
+    
+  <script type="text/javascript">
+        window.shopifyParams = {"testSecretKey": "test-key-3984dfj34f859gf45rtv4f54f", easyGatewyaPassword: "346AgAV8AJ4cM" ,"liveSecretKey": "live-key-3984dfj34f859gf4545g4g4545g5", "language": "sv-SE", "customerType" : "b2c" ,"merchantId": "100050065", "termsUrl": "https://terms-and-conditions.url"};
+    </script>  
   <script type="text/javascript" src="/js/bundle.js"></script></body>
   
-  <script> 
-      var firstState1 = ''
-  </script>
+   
 </html>
